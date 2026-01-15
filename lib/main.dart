aeacde697624ff7028edc39e3b08c1e8f0d2a266
mausamacharyaa <mausamsharmaa07@gmail.com>
2026-01-15 20:27:16 +0545

    learned about material app , scaffold,statelessand stateful widgits

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(app);
+void main(){
+  runApp(const Mausam());
+}
+
+class Mausam extends StatelessWidget {
+  const Mausam({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('Mausam Donna'),
+        
+      ),
+    );
+  }
 }
