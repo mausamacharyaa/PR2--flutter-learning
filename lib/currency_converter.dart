ed1f5bb35618ce2e707a5d6de83487e6ac3ea25c
mausamacharyaa <mausamsharmaa07@gmail.com>
2026-01-19 11:10:51 +0545

    Learned various stuffs

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
-  const CurrencyConverterMaterialPage({super.key});
+class CurrencyConverterMaterialPage extends StatefulWidget {
+  const CurrencyConverterMaterialPage({super.key});{
+    print('constructor');
+  }
+
+  @override
+  State<CurrencyConverterMaterialPage> createState() {
+    print('createState');
+    return _CurrencyConverterMaterialPageState();
+  }
+}
+
+class _CurrencyConverterMaterialPageState extends State<CurrencyConverterMaterialPage> {
   @override
   Widget build(BuildContext context) {
+    print('build fn');
+    double result = 0;
+
+    final TextEditingController textEditingController = TextEditingController();
     final border = OutlineInputBorder(
       borderSide: BorderSide(
         color: Colors.grey,
@@ -14,35 +29,55 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
       borderRadius: BorderRadius.all(Radius.circular(60)),
     );
     return Scaffold(
-      backgroundColor: Color.fromRGBO(0, 0, 0, 1),
+      backgroundColor: Color.fromRGBO(0, 45, 0, 1),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(0, 0, 0, 1),
+        elevation: 0,
+        title: const Text("Currency converter", style: TextStyle(color: Color.fromARGB(255, 63, 5, 5))),
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              '0',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600, color: Color.fromARGB(108, 40, 15, 15)),
+            Text(
+              result.toString(),
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w600, color: Color.fromARGB(108, 40, 15, 15)),
             ),
             Padding(
               padding: const EdgeInsets.all(10),
               child: TextField(
-                style: const TextStyle(color: Colors.white),
+                controller: textEditingController,
+                style: const TextStyle(color: Color.fromARGB(255, 146, 19, 19)),
                 decoration: InputDecoration(
                   hintText: 'Please Enter the Amount in USD',
-                  hintStyle: TextStyle(color: Colors.white),
+                  hintStyle: TextStyle(color: const Color.fromARGB(255, 201, 15, 15)),
                   prefix: const Icon(Icons.monetization_on),
                   prefixIconColor: Colors.green,
                   filled: true,
                   fillColor: Colors.green,
                   focusedBorder: border,
-                  enabledBorder: border, 
+                  enabledBorder: border,
                 ),
-                keyboardType: const TextInputType.numberWithOptions(
-                  decimal: true,
-                ),
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
               ),
             ),
-            
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextButton(
+                onPressed: () {
+                  result = double.parse(textEditingController.text) * 140;
+                  build(context);
+                },
+                style: ButtonStyle(
+                  backgroundColor: const WidgetStatePropertyAll(Colors.black),
+                  foregroundColor: const WidgetStatePropertyAll(Colors.cyanAccent),
+                  minimumSize: const WidgetStatePropertyAll(Size(double.infinity, 50)),
+                  shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                ),
+                child: const Text('Convert'),
+              ),
+            ),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class Mausam extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: CurrencyConverterMaterialPage(),
     );
   }
